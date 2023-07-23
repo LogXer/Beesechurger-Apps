eeb43f6ce483866d4eb47fcf143b51bd73caa919
LogXer <119995610+LogXer@users.noreply.github.com>
2023-07-23 18:35:16 -0500

    Update script.js

--- Generator/script.js
+++ Generator/script.js
@@ -5,7 +5,6 @@ document.getElementById('generate').addEventListener('click', function() {
   var historyLimit = document.getElementById('historyLimit').value;
   var historyList = document.getElementById('historyList');
   
-  // Clear previous random number(s)
   var randomNumberContainer = document.getElementById('randomNumbersContainer');
   while(randomNumberContainer.firstChild){
     randomNumberContainer.removeChild(randomNumberContainer.firstChild);
@@ -14,19 +13,15 @@ document.getElementById('generate').addEventListener('click', function() {
   for (var i = 0; i < num; i++) {
     var random = Math.floor(Math.random() * (max - min + 1)) + parseInt(min);
 
-    // Display the random number
     var span = document.createElement('span');
     span.textContent = random;
     randomNumberContainer.appendChild(span);
     
-    // Add the random number to history
     var li = document.createElement('li');
     li.textContent = random;
     historyList.prepend(li);
 
-    // Check if history list has more than historyLimit items
     while (historyList.children.length > historyLimit) {
-      // If it does, remove the last item
       historyList.removeChild(historyList.lastChild);
     }
   }
@@ -35,18 +30,14 @@ document.getElementById('generate').addEventListener('click', function() {
 document.getElementById('download').addEventListener('click', function() {
   var historyList = document.getElementById('historyList');
   
-  // Create a new Blob with the history list's text
   var blob = new Blob([Array.from(historyList.children).map(li => li.textContent).join('\n')], {type: 'text/plain'});
   
-  // Create a new object URL for the Blob
   var url = URL.createObjectURL(blob);
   
-  // Create a new anchor element and simulate a click to download the file
   var a = document.createElement('a');
   a.href = url;
   a.download = 'history.txt';
   a.click();
-  
-  // Revoke the object URL to free up memory
-  URL.revokeObjectURL(url);
+
+    URL.revokeObjectURL(url);
 });
